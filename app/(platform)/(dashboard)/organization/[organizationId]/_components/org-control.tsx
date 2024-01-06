121f8384b56e81081a0a28a5d3736909adee95ef
Sam Suh <samsuh@gmail.com>
2024-01-06 17:34:59 +0900

    create sidebar component and install shadcn skeleton and accordion. sidebar shows up now

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -1,9 +1,7 @@
 "use client" 
 
-
 import { useEffect } from "react"
 import { useParams } from "next/navigation"
-
 import { useOrganizationList } from "@clerk/nextjs"
 
 export const OrgControl = () => {
