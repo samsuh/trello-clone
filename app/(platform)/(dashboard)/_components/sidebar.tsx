1b3e7a469e7ecff0b6fbffc9edf1cc5618b4428c
Sam Suh <samsuh@gmail.com>
2024-01-08 04:55:19 +0900

    add needed imports and installations for sidebar component functionality

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,9 +1,27 @@
-"use client" 
+"use client"
 
-export const Sidebar = () => {
+import Link from "next/link"
+import { Plus } from "lucide-react"
+import { useLocalStorage } from "usehooks-ts"
+import { useOrganization, useOrganizationList } from "@clerk/nextjs"
+
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Accordion } from "@/components/ui/accordion"
+
+//storageKey used inside accordion; change sidebar items according to selected workspace
+interface SidebarProps {
+    storageKey?: string
+}
+
+export const Sidebar = ({storageKey}: SidebarProps) => {
     return (
-        <div>
-            Sidebar!
-        </div>
+        <>
+            <div className="font-medium text-xs flex items-center mb-1">
+                <span className="pl-4">Workspaces</span>
+                
+            </div>
+        </>
     )
 }
