8b887fee7a04c019c5d5fb69536777a49b817b83
Sam Suh <samsuh@gmail.com>
2024-01-09 17:47:32 +0900

    create sidebar state with useLocalStorage hook and create default accordion value

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -15,7 +15,39 @@ interface SidebarProps {
     storageKey?: string
 }
 
-export const Sidebar = ({storageKey}: SidebarProps) => {
+export const Sidebar = ({storageKey = 't-sidebar-state'}: SidebarProps) => {
+    // state tracking which accordion is expanded
+    const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(storageKey, {})
+
+    const {
+        organization: activeOrganization,
+        isLoaded: isLoadedOrg
+    } = useOrganization()
+
+    const {userMemberships, isLoaded: isLoadedOrgList} = useOrganizationList({
+        userMemberships: {infinite: true}
+    })
+
+    const defaultAccordionValue: string[] = Object.keys(expanded)
+        .reduce((acc: string[], key: string) => {
+            if (expanded[key]){
+                acc.push(key)
+            }
+            return acc
+        }, [])
+
+    const onExpand = (id: string) => {
+        setExpanded((curr) => ({...curr, [id]: !expanded[id]}))
+    }
+
+    if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading){
+        return (
+            <>
+                <Skeleton />
+            </>
+        )
+    }
+    
     return (
         <>
             <div className="font-medium text-xs flex items-center mb-1">
